d0edb8e228eb07ff4d78e942eb18efdf64096a10
RushiMore <rushi0957@gmail.com>
2026-01-17 17:52:51 +0530

    Day 7: MySQL + JPA integration

---
+++ demo/src/main/java/com/rushi/demo/entity/User.java
@@ -0,0 +1,47 @@
+package com.rushi.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- demo/src/main/java/com/rushi/demo/service/UserService.java
+++ demo/src/main/java/com/rushi/demo/service/UserService.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Service;
 
 import com.rushi.demo.UserRequest;
 import com.rushi.demo.UserResponse;
+import com.rushi.demo.entity.User;
+import com.rushi.demo.repository.UserRepository;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public String hello() {
         return "Hello! Spring Boot Day 2 🚀";
@@ -22,11 +30,16 @@ public class UserService {
         return "Current server time: " + LocalDateTime.now();
     }
 
-    public UserResponse createUser(UserRequest user) {
+    public UserResponse createUser(UserRequest userRequest) {
+
+        User user = new User(userRequest.getName(), userRequest.getAge());
+
+        User savedUser = userRepository.save(user);
+
         return new UserResponse(
-                "User created successfully",
-                user.getName(),
-                user.getAge()
+                "User saved successfully with ID: " + savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getAge()
         );
     }
 }
